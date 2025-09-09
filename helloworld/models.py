9f5e2fac4a16ca0b66e12eed13d61426a4f2ee15
Afnan Muhammad <otakutotipotent@gmail.com>
2025-09-09 16:45:31 +0500

    Add User model and register in admin
    Introduces a new User model with name, username, and date_added fields. Registers the User model in the Django admin interface and adds the initial migration for the new model.

--- helloworld/models.py
+++ helloworld/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    username = models.CharField(max_length=200, null=False, unique=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
